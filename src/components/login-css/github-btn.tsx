deff7759890d63a2748c9269cc32ddd0a9e49cf5
HANTAEDONG <htd0913@gmail.com>
2023-11-21 01:12:39 +0900

    2nd Commit

---
+++ src/components/login-css/github-btn.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+const Button = styled.span`
+
+`;
+
+
+export default function GitHubButton() {
+    return <Button>
+        <Logo src="/github-logo.svg" />
+        Continue with Github
+    </Button>
+}
