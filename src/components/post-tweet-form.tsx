90c8ee1d5d6136ef5622fca516c00b9cb07dfc9b
HANTAEDONG <htd0913@gmail.com>
2023-11-27 22:55:26 +0900

    tweet update까지 완료

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -1,13 +1,17 @@
+// 트윗창
+
 import { useState } from "react";
 import styled from "styled-components"
 
-const Form = styled.form`
+// 트윗 창의 form
+const TweetForm = styled.form`
     display: flex;
     flex-direction: column;
     gap: 10px;
 `;
 
-const TextArea = styled.textarea`
+// 트윗 창 testarea
+const TweetTextArea = styled.textarea`
     border:2px solid white;
     padding: 20px;
     border-radius: 20px;
@@ -16,17 +20,17 @@ const TextArea = styled.textarea`
     background-color: black;
     width: 100%;
     resize: None;
-    &::placeholder {
+    &::placeholder {    // placeholder의 폰트 바꿔주기, 가상요소 선택자
         font-size: 16px;
         font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     }
-    &:focus {
+    &:focus {   // 텍스트 영역이 focuse 됐을 때 설정
         outline: none;
         border-color: #1d9bf0;
     }
 `;
 
-const AttachFileButton = styled.label`
+const TweetAttachFileButton = styled.label`
      padding: 10px 0px;
      color: #1d9bf0;
      text-align: center;
@@ -37,10 +41,10 @@ const AttachFileButton = styled.label`
      cursor: pointer;
 `;
 
-const AttachFileInput = styled.input`
+const TweetAttachFileInput = styled.input`
   background-color: #1d9bf0;
   color: white;
-  border: none;
+  border: none; // input 테두리 안보이게
   padding: 10px 0px;
   border-radius: 20px;
   font-size: 16px;
@@ -51,14 +55,26 @@ const AttachFileInput = styled.input`
   }
 `;
 
-const SubmitBtn = styled.input``;
+const TweetSubmitBtn = styled.input`
+    background-color: #1d9bf0;
+    color: white;
+    border: none;
+    padding: 10px 0px;
+    border-radius: 20px;
+    font-size: 16px;
+    cursor: pointer;
+    &:hover,   // hover (마우스 오버 상태)
+    &:active {   // active (클릭하고 있는 상태)
+        opacity: 0.9;   // opacity는 투명도
+    }
+`;
 
 export default function PostTweetForm() {
     const [isLoading, setLoading] = useState(false);
-    const [tweet, setTweet] = useState("");
-    const [file, setFile] = useState<File | null>(null);
-    const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-        setTweet(e.target.value);
+    const [tweet, setTweet] = useState(""); // 컨텐츠 내용 저장하는 것
+    const [file, setFile] = useState<File | null>(null);    // 파일 저장하는 건데 File이나 Null 상태임
+    const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {   // textarea에서 일어나는 이벤트
+        setTweet(e.target.value);   // value의 값을 setTweet으로 변경?
     };
     const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { files } = e.target;
@@ -67,27 +83,30 @@ export default function PostTweetForm() {
         }
     };
     return (
-        <Form>
-            <TextArea
+        <TweetForm>
+            // 유저가 트윗하기 위한 텍트 area
+            <TweetTextArea
                 rows={5}
                 maxLength={180}
                 onChange={onChange}
                 value={tweet}
                 placeholder="What is happening?!"
             />
-            <AttachFileButton htmlFor="file">
+            // 유저가 사진 업로드 하기 위해 폴더에 접근하기 위한 버튼
+            <TweetAttachFileButton htmlFor="file">
                 {file ? "Photo added ✅" : "Add photo"}
-            </AttachFileButton>
-            <AttachFileInput
+            </TweetAttachFileButton>
+            <TweetAttachFileInput
                 onChange={onFileChange}
                 type="file"
                 id="file"
-                accept="image/*"
+                accept="image/*"    // 모든 이미지를 허용함.
             />
-            <SubmitBtn
+            // 서브밋 하기 위한 버튼
+            <TweetSubmitBtn
                 type="submit"
                 value={isLoading ? "Posting..." : "Post Tweet"}
             />
-        </Form>
+        </TweetForm>
     );
 }
--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
     apiKey: "AIzaSyB2huZUwASyslZvqEcRQkyA_GRfSwF14a0",
@@ -13,3 +15,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
+// 데이터 베이스와 스토리지 권한 얻기, 직접 접근 권한 얻음
+export const storage = getStorage(app);
+export const db = getDatabase(app);
